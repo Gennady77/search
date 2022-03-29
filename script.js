dfb70bb10f8e7d8f1d0b96e2679136252fc788e6
Gennadii Kondratev <gennadii.kondratev@gmail.com>
2022-03-29 10:33:54 +0300

    fix search element

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const btn = document.querySelector('.btn');
 const list = document.querySelectorAll('.li');
+const inputText = document.getElementById('inputText');
+const dropdownLayout = document.getElementById('dropdownLayout');
 
 
 btn.addEventListener('click', function() {
-    list[i].style.display = "block";
+    openDropdown();
 })
+
+inputText.addEventListener('input', function(e) {
+    openDropdown();
+});
+
+function openDropdown() {
+    dropdownLayout.style.display = 'block';
+}
